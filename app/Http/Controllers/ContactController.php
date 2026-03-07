e91c8588b65b7eebd79a1985c18e02e6f1205c6d
Elivelton <eliveltongsoficial@gmail.com>
2026-03-06 23:11:26 -0300

    Doc Swagger create post

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -7,12 +7,40 @@ use Application\Contact\DTO\ContactCreateDTO;
 use Application\Contact\UseCase\CreateContactUseCase;
 use Illuminate\Http\JsonResponse;
 
+#http://localhost:8080/api/documentation
+
+/**
+ * @OA\Info(
+ *     title="API of Contacts",
+ *     version="1.0"
+ * )
+ */
 class ContactController extends Controller
 {
     public function __construct(
         private CreateContactUseCase $createContactUseCase
     ) {}
 
+    /**
+     * @OA\Post(
+     *     path="/api/contacts",
+     *     summary="New Contact",
+     *     tags={"Contacts"},
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\JsonContent(
+     *             required={"name","email", "phone"},
+     *             @OA\Property(property="name", type="string", example="João"),
+     *             @OA\Property(property="email", type="string", example="joao@email.com"),
+     *             @OA\Property(property="phone", type="string", example="119999999")
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=201,
+     *         description="Created"
+     *     )
+     * )
+     */
     public function store(ContactRequest $request): JsonResponse
     {
         $dto = ContactCreateDTO::makeFromRequest($request->validated());
